cd30bcc10aa2fbb7f86975414d8799c832f9032e
root <fr3shnuts@gmail.com>
2017-09-05 13:01:40 -0700

    tabs2spaces

--- dirty_wifi.sh
+++ dirty_wifi.sh
@@ -31,21 +31,21 @@ while (( "$#" ))
 do
     if [[ $2 == "0" ]]
 	then
-	# Configure wpa_passphrase & configuration file
-	wpa_passphrase $1 $3 > /etc/wpa_supplicant/auto.conf 
-	wpa_supplicant -B -Dwext -i$4 -c/etc/wpa_supplicant/auto.conf
+	    # Configure wpa_passphrase & configuration file
+	    wpa_passphrase $1 $3 > /etc/wpa_supplicant/auto.conf 
+	    wpa_supplicant -B -Dwext -i$4 -c/etc/wpa_supplicant/auto.conf
 
-    	# Allow time for proper connection before asking for IP from DHCP Server
-    	sleep 1
-    	echo -en "DHCP client reset\n"
-    	dhclient -r $4
+        # Allow time for proper connection before asking for IP from DHCP Server
+        sleep 1
+        echo -en "DHCP client reset\n"
+        dhclient -r $4
 
-    	echo -en "Obtaining IP address\n"
-    	dhclient $4
-    	sleep 3
+        echo -en "Obtaining IP address\n"
+        dhclient $4
+        sleep 3
 
-    	echo -en "\n*** Got Internet? ***\n"
-		break
+        echo -en "\n*** Got Internet? ***\n"
+	    break
 	fi
 
 #
@@ -54,24 +54,24 @@ do
 
 	if [[ $2 == "1" ]]
 	then
-    	echo -en "network={\n" > /etc/wpa_supplicant/open.conf
-    	echo -en "        ssid=\"$1\"\n" >> /etc/wpa_supplicant/open.conf
-    	echo -en "        key_mgmt=NONE\n" >> /etc/wpa_supplicant/open.conf
-    	echo -en "}\n" >> /etc/wpa_supplicant/open.conf
+        echo -en "network={\n" > /etc/wpa_supplicant/open.conf
+        echo -en "        ssid=\"$1\"\n" >> /etc/wpa_supplicant/open.conf
+        echo -en "        key_mgmt=NONE\n" >> /etc/wpa_supplicant/open.conf
+        echo -en "}\n" >> /etc/wpa_supplicant/open.conf
 
-    	wpa_supplicant -B -Dwext -i$3 -c/etc/wpa_supplicant/open.conf
+        wpa_supplicant -B -Dwext -i$3 -c/etc/wpa_supplicant/open.conf
 
-    	sleep 1
-    	echo -en "dhcp client reset\n"
-    	dhclient -r $3
+        sleep 1
+        echo -en "dhcp client reset\n"
+        dhclient -r $3
 
-    	sleep 1
-    	echo -en "Obtaining IP Address.\n"
-    	dhclient $3
-    	sleep 3
+        sleep 1
+        echo -en "Obtaining IP Address.\n"
+        dhclient $3
+        sleep 3
 
-    	echo -en "\n*** Got Internet?***\n"
-		break
+        echo -en "\n*** Got Internet?***\n"
+	    break
 	fi
 
 	if [ $2 != "0" ] || [ $2 != "1" ]
